06a7b4ae50658272bcaac191f7367898b0bb42ab
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-02-07 21:52:32 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,35 @@
+import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import { Layout } from './pages/00Layout';
 import { Home } from './pages/02Home';
-
 import { Movies } from './pages/03Movies';
 import { MovieDetails } from './pages/04MovieDetails';
 import { Cast } from './pages/05Cast';
 import { Reviews } from './pages/06Reviews';
 import { NotFound } from './pages/NotFound';
-
+// const Layout = lazy(() => import('./pages/00Layout'));
+// const Home = lazy(() => import('./pages/02Home'));
+// const Movies = lazy(() => import('./pages/03Movies'));
 // const MovieDetails = lazy(() => import('./pages/04MovieDetails'));
 // const Cast = lazy(() => import('./pages/05Cast'));
 // const Reviews = lazy(() => import('./pages/06Reviews'));
 // const NotFound = lazy(() => import('./pages/NotFound'));
-
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="review" element={<Reviews />} />
+      <Suspense fallback={<div>'Loading...'</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="review" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
           </Route>
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </>
   );
 };
